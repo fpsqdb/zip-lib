3f99d42235eeccbcfcc06ea517110e13c32dd05d
fpsqdb <fpsqdb@gmail.com>
2019-10-12 21:35:29 +0800

    test case: archive an exist zip file.

---
+++ test/src/zipExistArchiveFile.ts
@@ -0,0 +1,22 @@
+import * as zl from "../../lib"
+import * as path from "path";
+import * as assert from "assert";
+
+describe("zip", () => {
+    it("zip a archive file twice", async () => {
+        try {
+            const twiceFile = path.join(__dirname, "../zips/resources_zip_twice.zip");
+            await zl.archiveFile(path.join(__dirname, "../unzipResources/resources.zip"), twiceFile);
+            await zl.extract(twiceFile, path.join(__dirname, "../zips/resources_zip_twice"), {
+                overwrite: true
+            });
+            const resourcesZip = path.join(__dirname, "../zips/resources_zip_twice/resources.zip");
+            await zl.extract(resourcesZip, path.join(__dirname, "../zips/resources_zip_twice/resources"), {
+                overwrite: true
+            });
+            assert.ok(true, "zip a archive file twice");
+        } catch (error) {
+            assert.fail(error);
+        }
+    });
+});

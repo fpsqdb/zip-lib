f56bdc8f190afecb0bdff0739d29a9bd5b257dc3
fpsqdb <fpsqdb@gmail.com>
2019-10-12 09:43:47 +0800

    Create an `empty folder` before executing test.

--- test/src/before.js
+++ test/src/before.js
@@ -6,6 +6,11 @@ if(!fs.existsSync(folder1)){
     fs.mkdirSync(folder1);
 }
 
+const subfolder1 = path.join(folder1, "/empty folder")
+if(!fs.existsSync(subfolder1)){
+    fs.mkdirSync(subfolder1);
+}
+
 const folder2 = path.join(__dirname, "../resources/«ταБЬℓσ»")
 if(!fs.existsSync(folder2)){
     fs.mkdirSync(folder2);

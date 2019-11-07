67a7b377f6902ddde02bebdd596a5ef2c269b917
fpsqdb <fpsqdb@gmail.com>
2019-11-07 22:17:59 +0800

    Since drop support for node.js < 8, use `promisify` everywhere

--- src/util.ts
+++ src/util.ts
@@ -1,78 +1,38 @@
 import * as fs from "fs";
 import * as util from "util";
 
-function asyncCall(fn: Function, ...args: any[]): any {
-    return new Promise((c, e) => fn(...args, (err: any, result: any) => err ? e(err) : c(result)));
-}
-
 export function unlink(path: fs.PathLike): Promise<void> {
-    if (typeof util.promisify === "function") {
-        return util.promisify(fs.unlink)(path);
-    } else {
-        return asyncCall(fs.unlink, path);
-    }
+    return util.promisify(fs.unlink)(path);
 }
 
 export function mkdir(path: fs.PathLike, mode?: string | number | null | undefined): Promise<void> {
-    if (typeof util.promisify === "function") {
-        return util.promisify(fs.mkdir)(path, mode);
-    } else {
-        return asyncCall(fs.mkdir, path, mode);
-    }
+    return util.promisify(fs.mkdir)(path, mode);
 }
 
 export function lstat(path: fs.PathLike): Promise<fs.Stats> {
-    if (typeof util.promisify === "function") {
-        return util.promisify(fs.lstat)(path);
-    } else {
-        return asyncCall(fs.lstat, path);
-    }
+    return util.promisify(fs.lstat)(path);
 }
 
 export function chmod(path: fs.PathLike, mode: string | number): Promise<void> {
-    if (typeof util.promisify === "function") {
-        return util.promisify(fs.chmod)(path, mode);
-    } else {
-        return asyncCall(fs.chmod, path, mode);
-    }
+    return util.promisify(fs.chmod)(path, mode);
 }
 
 export function readdir(path: fs.PathLike): Promise<string[]> {
-    if (typeof util.promisify === "function") {
-        return util.promisify(fs.readdir)(path);
-    } else {
-        return asyncCall(fs.readdir, path);
-    }
+    return util.promisify(fs.readdir)(path);
 }
 
 export function access(path: fs.PathLike, mode?: number | undefined): Promise<void> {
-    if (typeof util.promisify === "function") {
-        return util.promisify(fs.access)(path, mode);
-    } else {
-        return asyncCall(fs.access, path, mode);
-    }
+    return util.promisify(fs.access)(path, mode);
 }
 
 export function rmdir(path: fs.PathLike): Promise<void> {
-    if (typeof util.promisify === "function") {
-        return util.promisify(fs.rmdir)(path);
-    } else {
-        return asyncCall(fs.rmdir, path);
-    }
+    return util.promisify(fs.rmdir)(path);
 }
 
 export function symlink(target: fs.PathLike, path: fs.PathLike): Promise<void> {
-    if (typeof util.promisify === "function") {
-        return util.promisify(fs.symlink)(target, path);
-    } else {
-        return asyncCall(fs.symlink, target, path);
-    }
+    return util.promisify(fs.symlink)(target, path);
 }
 
 export function readlink(path: fs.PathLike): Promise<string> {
-    if (typeof util.promisify === "function") {
-        return util.promisify(fs.readlink)(path);
-    } else {
-        return asyncCall(fs.readlink, path);
-    }
+    return util.promisify(fs.readlink)(path);
 }
